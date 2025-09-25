ae910bf8931648d8a2e86c3fb79d61a9c87c7f4a
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-09-25 19:15:37 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -185,6 +185,6 @@ if uploaded_old and uploaded_new:
 st.markdown("""
     <hr style='margin-top: 40px;'>
     <div style='text-align: center; font-size: 14px; color: gray;'>
-        · Built with ❤️ using Streamlit · Resume parsing enabled · OpenAI-powered rewriting · Google Sheets logging active · Recruiter metrics visualized · Drift monitoring integrated ·
+        • Built with ❤️ using Streamlit • Resume parsing enabled • OpenAI-powered rewriting • Google Sheets logging active • Recruiter metrics visualized • Drift monitoring integrated •
     </div>
 """, unsafe_allow_html=True)
