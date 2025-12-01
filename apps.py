00b354792ba78bcb4685deac76863af99932ccc9
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-12-01 18:46:35 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -307,7 +307,7 @@ logo = load_logo_base64()
 if logo:
     st.markdown(f"<div style='text-align:center'><img src='data:image/png;base64,{logo}' width='250'></div>", unsafe_allow_html=True)
 
-st.title("JobBot (Data Scientist & Analytics Engineer)")
+st.title("JobBot (Data Scientist & Analytics)")
 
 
 ### OPTIONS
@@ -400,4 +400,5 @@ if df is not None and not df.empty:
 
 ### FOOTER
 st.markdown("<hr><center>JobBot+ Full Edition — Built for Vikrant</center>", unsafe_allow_html=True)
+
 
